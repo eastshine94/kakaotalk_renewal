b03a9de6dce84894b2350a0337bc972ab97b5aba
hdkim <eastshine94@naver.com>
2022-04-20 15:38:35 +0900

    GraphQL Test

--- pages/graph.tsx
+++ pages/graph.tsx
@@ -2,39 +2,39 @@
 import { NextPage } from 'next';
 import React from 'react';
 import { useQuery } from 'react-query';
-import { request, gql } from 'graphql-request';
+import { request, gql, ClientError } from 'graphql-request';
 
 const Graph: NextPage = () => {
+  const tag = gql`
+    query {
+      user(user_id: "test1@test.com") {
+        user_id
+        name
+      }
+    }
+  `;
   const fetchData = async () => {
-    const res = await request<{
-      user: Partial<{ user_id: string; password: string; name: string }>;
-    }>(
-      'http://localhost:3001/graphql',
-      gql`
-        query {
-          user(id: 71) {
-            user_id
-            name
-          }
-        }
-      `
-    );
-    console.log(res);
-    return res;
+    try {
+      const res = await request<{
+        user: Partial<{ user_id: string; password: string; name: string }>;
+      }>('http://localhost:3001/graphql', tag);
+
+      return res;
+    } catch (err) {
+      const error = err as ClientError;
+      console.dir(error.response.errors?.[0].message);
+    }
   };
 
   const { data } = useQuery(['user'], fetchData);
-
-  if (data) {
-    console.log(data.user.user_id);
-  }
 
   return (
     <div>
       {data && (
         <div>
-          <div>{data.user.name}</div>
-          <div>{data.user.user_id}</div>
+          <div>{data.user?.name}</div>
+          <div>{data.user?.user_id}</div>
+          <div>{data.user?.password}</div>
         </div>
       )}
     </div>
